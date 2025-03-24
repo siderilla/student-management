a46ee4b8360fcd4bf5af7eec44cea908833e3d15
siderilla <siderilla11@gmail.com>
2025-03-24 13:11:14 +0100

    added folders

--- src/services/main.js
+++ src/services/main.js
@@ -1,9 +1,9 @@
-import './super-header.js';
-import './super-footer.js';
-import './student-card.js';
-import './super-grid.js';
-import './student-add.js';
-import './student-edit.js';
+import '../components/super-header.js';
+import '../components/super-footer.js';
+import '../components/student-card.js';
+import '../components/super-grid.js';
+import '../components/student-add.js';
+import '../components/student-edit.js';
 import StudentService from './student-service.js';
 
 const service = new StudentService();
